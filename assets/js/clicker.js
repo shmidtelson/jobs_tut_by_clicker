8c55fdb567306fe2804334b2df939543b006dc59
Romua1d Shmidtelson <d-w-b@mail.ru>
2019-02-18 11:38:38 +0300

    updated

--- assets/js/clicker.js
+++ assets/js/clicker.js
@@ -2,6 +2,8 @@ var $input = document.querySelectorAll('.bloko-icon-link');
 moment.locale('ru');
 
 var parsePage = class parsePage {
+    static current_selector = '.resumelist__update-info';
+
     constructor() {
         this.link_clicker()
     }
@@ -15,9 +17,15 @@ var parsePage = class parsePage {
     }
 
     static getLatestTime() {
-        var $date = document.querySelector('.resumelist__update-info');
+        var $date = document.querySelector(this.current_selector);
         // $date - 7 февраля 2019 23:50
+        // Разбираем дату в массив.
         var arr = $date.innerText.split(" "); // Split date for parsing
+        /*
+         Cобираем дату в нужном нам формате. Чтобы не путаться с окончаниями, мы обрезаем название текущего месяца
+         до первых трех символов: февраля => фев. И снова собираем дату и добавляем к ней 4 часа (таков интервал
+         системы) и трансформируем ее в UNIX дату, чтобы можно было комфортно сравнивать Int.
+          */
         $date = moment(arr[0] + " " + arr[1].substring(0,3) + " " + arr[2] + " " + arr[3], 'D MMM YYYY HH:mm')
             .add(4, 'hours') // For condition
             .format('X');
