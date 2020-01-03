bbf828604b797b9b0ee8bb34462cdd950295cb70
romua1d <d-w-b@mail.ru>
2020-01-03 20:21:19 +0300

    Update by new design

--- assets/js/clicker.js
+++ assets/js/clicker.js
@@ -1,52 +1,16 @@
-var $input = document.querySelectorAll('.bloko-icon-link');
-moment.locale('ru');
-
 var parsePage = class parsePage {
-    static current_selector = '.resumelist__update-info';
-
-    constructor() {
-        this.link_clicker()
-    }
-
-    static reloadPage() {
-        window.location.reload()
-    }
-
-    static nowDate() {
-        return parseInt(moment().format('X'))
-    }
-
-    static getLatestTime() {
-        var $date = document.querySelector(this.current_selector);
-        // $date - 7 февраля 2019 23:50
-        // Разбираем дату в массив.
-        var arr = $date.innerText.split(" "); // Split date for parsing
-        /*
-         Cобираем дату в нужном нам формате. Чтобы не путаться с окончаниями, мы обрезаем название текущего месяца
-         до первых трех символов: февраля => фев. И снова собираем дату и добавляем к ней 4 часа (таков интервал
-         системы) и трансформируем ее в UNIX дату, чтобы можно было комфортно сравнивать Int.
-          */
-        $date = moment(arr[0] + " " + arr[1].substring(0,3) + " " + arr[2] + " " + arr[3], 'D MMM YYYY HH:mm')
-            .add(4, 'hours') // For condition
-            .format('X');
-        return parseInt($date);
-    }
-
+    targetsQuery = '.applicant-resumes-update-button:not(.applicant-resumes-update-button_disabled) .bloko-link[data-qa="resume-update-button"]';
     link_clicker() {
-        if (parsePage.nowDate() > parsePage.getLatestTime()) {
-            $input.forEach((element) => {
-                if (element.disabled !== null && !element.disabled) {
-                    element.click();
-                    console.log('clicked');
-                }
-            });
-            parsePage.reloadPage();
-        } else {
-            console.log(moment().format("MMMM D YYYY, HH:mm"));
-        }
+        var targetItems = document.querySelectorAll(this.targetsQuery);
+
+        targetItems.forEach(function (item) {
+            item.click()
+        });
+
+        window.location.reload()
     };
 };
 
-var p = new parsePage();
-
-setInterval(p.link_clicker, 60000);
+setInterval(function () {
+    (new parsePage()).link_clicker();
+}, 60000);
